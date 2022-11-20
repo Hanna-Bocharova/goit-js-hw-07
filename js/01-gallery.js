b437f3127aca253bc4da6939f2f5b77bac121479
Hanna Bocharova <106443943+Hanna-Bocharova@users.noreply.github.com>
2022-11-20 12:00:14 +0300

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-console.log(createGalleryImg(items));
+console.log(createGalleryImg(galleryItems));
 
-function createGalleryImg(items) {
-const gallery = galleryItems.map(galleryItem => {
+// const instance = basicLightbox.create(`
+//      <div class="modal">
+//      <a class="gallery__link"
+//      href="${original}">
+//      <img class="gallery__image"
+//      src="${preview}" 
+//      alt="${description}"
+//      data-source="${original}"
+//      />
+//      </a>
+//      </div>
+//  `);
+
+const imgContainer = document.querySelector(`.gallery__item`);
+const galleryToDo = createGalleryImg(galleryItems);
+imgContainer.insertAdjacentHTML(`beforeend`, galleryToDo);
+imgContainer.addEventListener(`click`, onImgContainerClick)
+
+function createGalleryImg(galleryItems) {
+return galleryItems.map(({preview, original, description}) => {
         return `
     <div class="gallery__item">
         <a class="gallery__link"
@@ -14,12 +32,21 @@ const gallery = galleryItems.map(galleryItem => {
         data-source="${original}"
         />
         </a>
-    </div>`
-    });
-    console.log(gallery);
+    </div>`;
+    })
+    .join("");
+    
 }
 
+function onImgContainerClick(evt) {
+    evt.preventDefault();
+    const isimgEl = evt.target.classList.contains(`.gallery__image`);
+    if(!isimgEl){ 
+        return
+    }
+    
 
+}
 
 
 
